7f522965fa1a9205aadb7b039cd4bb808356fffa
enzo-cys <enzo.cys@laplateforme.io>
2025-09-19 13:11:45 +0200

    Dernière version du job 7

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,48 +1,74 @@
-<!-- Job 07 : Formulaire maison -->
-<form method="get">
-  <input type="text" name="largeur" placeholder="Largeur">
-  <input type="text" name="hauteur" placeholder="Hauteur">
-  <input type="submit" value="Construire la maison">
-</form>
-<pre>
 <?php
-// Job 07 : Afficher une maison en ASCII art
-if (isset($_GET['largeur']) && isset($_GET['hauteur'])) {
-    $largeur = $_GET['largeur']; 
-    $hauteur = $_GET['hauteur']; 
-// Vérification des valeurs minimales
-    if ($largeur < 3 || $hauteur < 2) {
-        echo "Largeur minimum : 3, hauteur minimum : 2";
-        exit;
-    }
-// Dessin de la maison
-    for ($i = 0; $i < $largeur - 1; $i++) {
-        for ($j = 0; $j < $largeur - $i - 1; $j++) echo " ";
-        echo "/";
-        for ($j = 0; $j < $i * 2; $j++) echo "-";
-        echo "\\";
-        echo "\n";
-    }
+echo "<form method='post'>";
+echo "Largeur: <input type='text' name='largeur' required>";
+echo "Hauteur: <input type='text' name='hauteur' required>";
+echo "<button type='submit'>Submit</button>";
+echo "</form>";
 
-  // Toit de la maison
-    echo "/";
-    for ($i = 0; $i < ($largeur - 1) * 2; $i++) echo "-";
-    echo "\\";
-    echo "\n";
-
-   // Murs de la maison
-    for ($i = 0; $i < $hauteur; $i++) {
-        echo "|";
-        for ($j = 0; $j < ($largeur - 1) * 2; $j++) echo " ";
-        echo "|";
-        echo "\n";
+if (isset($_POST['largeur']) && isset($_POST['hauteur'])) {
+    // Conversion 
+    $largeur = 0;
+    $hauteur = 0;
+    $l_str = $_POST['largeur'];
+    $h_str = $_POST['hauteur'];
+    for ($i = 0; $i < strlen($l_str); $i++) {
+        $c = $l_str[$i];
+        if ($c >= '0' && $c <= '9') {
+            $largeur = $largeur * 10 + (ord($c) - ord('0'));
+        }
+    }
+    for ($i = 0; $i < strlen($h_str); $i++) {
+        $c = $h_str[$i];
+        if ($c >= '0' && $c <= '9') {
+            $hauteur = $hauteur * 10 + (ord($c) - ord('0'));
+        }
     }
 
-    // Sol de la maison
-    echo "|";
-    for ($i = 0; $i < ($largeur - 1) * 2; $i++) echo "_";
-    echo "|";
-    echo "\n";
+    if ($largeur > 2 && $hauteur > 1) {
+        $maison = "";
+
+        // Toit 
+        for ($i = 0; $i < $hauteur; $i++) {
+            // Espaces
+            for ($j = 0; $j < $hauteur - $i - 1; $j++) {
+                $maison .= " ";
+            }
+            $maison .= "/";
+
+            // Underscores
+            $underscores = $largeur - 2 * ($hauteur - $i - 1);
+            if ($underscores < 1) $underscores = 1;
+            if ($underscores > 1) {
+                for ($k = 0; $k < $underscores - 2; $k++) {
+                    $maison .= "_";
+                }
+            }
+
+            $maison .= "\\" . "\n";
+        }
+
+        // maison
+        for ($j = 0; $j < $hauteur; $j++) {
+            if ($j == $hauteur - 1) {
+                // Sol du carré
+                $maison .= "|";
+                for ($k = 0; $k < $largeur - 2; $k++) {
+                    $maison .= "_";
+                }
+                $maison .= "|\n";
+            } else {
+                // Mur du carré
+                $maison .= "|";
+                for ($k = 0; $k < $largeur - 2; $k++) {
+                    $maison .= " ";
+                }
+                $maison .= "|\n";
+            }
+        }
+
+        echo "<pre>$maison</pre>";
+    } else {
+        echo "Veuillez entrer une largeur > 2 et une hauteur > 1.";
+    }
 }
 ?>
-</pre>
